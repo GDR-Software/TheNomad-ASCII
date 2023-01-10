7e7ca2b2dbc269115368e6aeb7126e6e5ccf5ca1
SIGAAMDAD <hankburne@gmail.com>
2023-01-09 18:10:44 -0600

    updating combat code

--- src/g_combat.cpp
+++ src/g_combat.cpp
@@ -37,7 +37,11 @@ void P_ShootShotty(Weapon* const wpn)
 {
 	nomadenum_t spread = wpn->c_wpn.spread;
 	nomadenum_t numpellets = wpn->c_wpn.numpellets;
-	nomadenum_t a{};
+	nomaduint_t range = wpn->c_wpn.range;
+	nomadshort_t a{}, y, x, i, p;
+	coord_t slope, d;
+
+	d = game->E_GetDir();
 
 	coord_t maxspread[2]; // 0 -> left, 1 -> right
 	switch (playr->pdir) {
@@ -45,21 +49,25 @@ void P_ShootShotty(Weapon* const wpn)
 		maxspread[0].y = maxspread[1].y = playr->pos.y;
 		maxspread[0].x = playr->pos.x - (spread >> 1);
 		maxspread[1].x = playr->pos.x + (spread >> 1);
+		p = playr->pos.y;
 		break;
 	case D_WEST:
 		maxspread[0].x = maxspread[1].x = playr->pos.x;
 		maxspread[0].y = playr->pos.y + (spread >> 1);
 		maxspread[1].y = playr->pos.y - (spread >> 1);
+		p = playr->pos.x;
 		break;
 	case D_SOUTH:
 		maxspread[0].y = maxspread[1].y = playr->pos.y;
 		maxspread[0].x = playr->pos.x + (spread >> 1);
 		maxspread[1].x = playr->pos.x + (spread >> 1);
+		p = playr->pos.y;
 		break;
 	case D_EAST:
 		maxspread[0].x = maxspread[1].x = playr->pos.x;
 		maxspread[0].y = playr->pos.y - (spread >> 1);
 		maxspread[1].y = playr->pos.y + (spread >> 1);
+		p = playr->pos.x;
 		break;
 	default:
 		N_Error("Unknown/Invalid Player Direction: %hu", playr->pdir);
@@ -67,13 +75,40 @@ void P_ShootShotty(Weapon* const wpn)
 	};
 
 	nomadshort_t offset;
-	for (nomadushort_t o = 0; o < numpellets; ++o) {
+	nomadushort_t o;
+	for (o = 0; o < numpellets; ++o) {
 		a = P_Random() & 4;
 		if (a > 2)
 			offset = -P_Random() & -2;
 		else
 			offset = P_Random() & 2;
-
-
+		
+		slope.y = maxspread[(P_Random() & 1)].y + offset;
+		slope.x = maxspread[(P_Random() & 1)].x + offset;
+		for (y = playr->pos.y;; y += slope.y) {
+			for (x = playr->pos.x;; x += slope.x) {
+				// FIXME - i have ideas on how to optimize this
+				if (playr->pdir == D_NORTH && y < (range - playr->pos.y)) {
+					break;
+				}
+				else if (playr->pdir == D_WEST && x < (range - playr->pos.x)) {
+					break;
+				}
+				else if (playr->pdir == D_SOUTH && y > (range + playr->pos.y)) {
+					break;
+				}
+				else if (playr->pdir == D_EAST && x > (range + playr->pos.x)) {
+					break;
+				}
+				switch (game->c_map[y][x]) {
+				case ' ':
+				case '.':
+					break;
+				case '#':
+					// do something
+					break;
+				};
+			}
+		}
 	}
 }
