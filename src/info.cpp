4fe84d58c97118b65d08e333d794c6a117757d0c
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2023-02-08 13:04:54 -0600

    Update info.cpp

--- src/info.cpp
+++ src/info.cpp
@@ -50,7 +50,7 @@ scf::keybind_t scf::kb_binds[scf::NUMBINDS] = {
 	{"Slide West",     KEY_A,       scf::kbSlide_w,    P_SlideW},
 	{"Slide South",    KEY_S,       scf::kbSlide_s,    P_SlideS},
 	{"Slide East",     KEY_D,       scf::kbSlide_e,    P_SlideE},
-	{"Interact",       KEY_ENTER,   scf::kbInteract,   P_Interact}
+	{"Interact",       KEY_i,       scf::kbInteract,   P_Interact}
 };
 
 std::vector<npc_t> npcinfo = {
@@ -59,53 +59,260 @@ std::vector<npc_t> npcinfo = {
 	{"Weapons Smith\0",    'w', 100, 43}
 };
 
-const entitystate_t stateinfo[NUMSTATES] = {
-	{S_PLAYR_NULL,      0},
-	{S_PLAYR_SPAWN,     2},
-	{S_PLAYR_MOVE,      5},
-	{S_PLAYR_IDLE,      3},
-	{S_PLAYR_SHOOT,     6},
-	{S_PLAYR_MELEE,     8},
-	{S_PLAYR_INTERACT,  10},
-	{S_PLAYR_LOWHEALTH, 24},
-	{S_PLAYR_DEAD,      ticrate_base},
-	{S_MOB_NULL,        0},
-	{S_MOB_SPAWN,       15},
-	{S_MOB_WANDER,      16},
-	{S_MOB_IDLE,        ticrate_base*2},
-	{S_MOB_CHASEPLAYR,  ticrate_base*25},
-	{S_MOB_FIGHT,       ticrate_base*15},
-	{S_MOB_FLEE,        15},
-	{S_MOB_DEAD,        65},
-	{S_BOT_NULL,        0},
-	{S_BOT_SPAWN,       10},
-	{S_BOT_WANDER,      ticrate_base*2},
-	{S_BOT_IDLE,        ticrate_base*3},
-	{S_BOT_INTERACTING, ticrate_base*6},
-	{S_BOT_DEAD,        10},
+const entitystate_t pstateinfo[NUMPSTATES] = {
+	{S_PLAYR_NULL, 0, 0, NULL},
+	{S_PLAYR_SPAWN, 2, 0, NULL},
+	{S_PLAYR_MOVE, 5, 0, NULL},
+	{S_PLAYR_IDLE, 3, 0, NULL},
+	{S_PLAYR_SHOOT, 6, 0, NULL},
+	{S_PLAYR_MELEE, 8, 0, NULL},
+	{S_PLAYR_INTERACT, 10, 0, NULL},
+	{S_PLAYR_LOWHEALTH, 24, 0, NULL},
+	{S_PLAYR_DEAD, ticrate_base, S_PLAYR_NULL, NULL},
+	
+	// mob states
+	{S_HULK_NULL,              0,               S_HULK_SPAWN,         NULL}, // S_HULK_NULL
+	{S_HULK_SPAWN,             5,               S_HULK_WANDER,        M_SpawnThink}, // S_HULK_SPAWN
+	{S_HULK_WANDER,            ticrate_base*4,  S_HULK_IDLE,          M_WanderThink}, // S_HULK_WANDER
+	{S_HULK_IDLE,              ticrate_base*3,  S_HULK_WANDER,        M_IdleThink}, // S_HULK_IDLE
+	{S_HULK_CHASEPLAYR,        ticrate_base*5,  S_HULK_FIGHT,         M_ChasePlayr}, // S_HULK_CHASEPLAYR
+	{S_HULK_FIGHT,             ticrate_base*2,  S_HULK_WANDER,        M_FightThink}, // S_HULK_FIGHT
+	{S_HULK_FLEE,              ticrate_base*4,  S_HULK_WANDER,        M_FleeThink}, // S_HULK_FLEE
+	{S_HULK_DEAD,              ticrate_base*10, S_HULK_NULL,          M_DeadThink}, // S_HULK_DEAD
+	{S_RAV_NULL,               0,               S_RAV_SPAWN,          NULL}, // S_RAV_NULL
+	{S_RAV_SPAWN,              5,               S_RAV_WANDER,         M_SpawnThink}, // S_RAV_SPAWN
+	{S_RAV_WANDER,             ticrate_base,    S_RAV_IDLE,           M_WanderThink}, // S_RAV_WANDER
+	{S_RAV_IDLE,               ticrate_base,    S_RAV_WANDER,         M_IdleThink}, // S_RAV_IDLE
+	{S_RAV_CHASEPLAYR,         ticrate_base,    S_RAV_FIGHT,          M_ChasePlayr}, // S_RAV_CHASEPLAYR
+	{S_RAV_FIGHT,              ticrate_base,    S_RAV_WANDER,         M_FightThink}, // S_RAV_FIGHT
+	{S_RAV_FLEE,               ticrate_base,    S_RAV_WANDER,         M_FleeThink}, // S_RAV_FLEE
+	{S_RAV_DEAD,               ticrate_base,    S_RAV_NULL,           M_DeadThink}, // S_RAV_DEAD
+	{S_GRUNT_NULL,             0,               S_GRUNT_SPAWN,        NULL}, // S_GRUNT_NULL
+	{S_GRUNT_SPAWN,            5,               S_GRUNT_WANDER,       M_SpawnThink}, // S_GRUNT_SPAWN
+	{S_GRUNT_WANDER,           ticrate_base,    S_GRUNT_IDLE,         M_WanderThink}, // S_GRUNT_WANDER
+	{S_GRUNT_IDLE,             ticrate_base,    S_GRUNT_WANDER,       M_IdleThink}, // S_GRUNT_IDLE
+	{S_GRUNT_CHASEPLAYR,       ticrate_base,    S_GRUNT_FIGHT,        M_ChasePlayr}, // S_GRUNT_CHASEPLAYR
+	{S_GRUNT_FIGHT,            ticrate_base,    S_GRUNT_WANDER,       M_FightThink}, // S_GRUNT_FIGHT
+	{S_GRUNT_FLEE,             ticrate_base,    S_GRUNT_WANDER,       M_FleeThink}, // S_GRUNT_FLEE
+	{S_GRUNT_DEAD,             ticrate_base,    S_GRUNT_NULL,         M_DeadThink}, // S_GRUNT_DEAD
+	{S_DRUID_NULL,             0,               S_DRUID_SPAWN,        NULL}, // S_DRUID_NULL
+	{S_DRUID_SPAWN,            5,               S_DRUID_WANDER,       M_SpawnThink}, // S_DRUID_SPAWN
+	{S_DRUID_WANDER,           ticrate_base,    S_DRUID_IDLE,         M_WanderThink}, // S_DRUID_WANDER
+	{S_DRUID_IDLE,             ticrate_base,    S_DRUID_WANDER,       M_IdleThink}, // S_DRUID_IDLE
+	{S_DRUID_CHASEPLAYR,       ticrate_base,    S_DRUID_FIGHT,        M_ChasePlayr}, // S_DRUID_CHASEPLAYR
+	{S_DRUID_FIGHT,            ticrate_base,    S_DRUID_WANDER,       M_FightThink}, // S_DRUID_FIGHT
+	{S_DRUID_FLEE,             ticrate_base,    S_DRUID_WANDER,       M_FleeThink}, // S_DRUID_FLEE
+	{S_DRUID_DEAD,             ticrate_base,    S_DRUID_NULL,         M_DeadThink}, // S_DRUID_DEAD
+	{S_SANDWURM_NULL,          0,               S_SANDWURM_SPAWN,     NULL}, // S_RAV_NULL
+	{S_SANDWURM_SPAWN,         5,               S_SANDWURM_WANDER,    M_SpawnThink}, // S_SANDWURM_SPAWN
+	{S_SANDWURM_WANDER,        ticrate_base,    S_SANDWURM_IDLE,      M_WanderThink}, // S_SANDWURM_WANDER
+	{S_SANDWURM_IDLE,          ticrate_base,    S_SANDWURM_WANDER,    M_IdleThink}, // S_SANDWURM_IDLE
+	{S_SANDWURM_CHASEPLAYR,    ticrate_base,    S_SANDWURM_FIGHT,     M_ChasePlayr}, // S_SANDWURM_CHASEPLAYR
+	{S_SANDWURM_FIGHT,         ticrate_base,    S_SANDWURM_WANDER,    M_FightThink}, // S_SANDWURM_FIGHT
+	{S_SANDWURM_FLEE,          ticrate_base,    S_SANDWURM_WANDER,    M_FleeThink}, // S_SANDWURM_FLEE
+	{S_SANDWURM_DEAD,          ticrate_base,    S_SANDWURM_NULL,      M_DeadThink}, // S_SANDWURM_DEAD
+	{S_MERC_NULL,              0,               S_MERC_SPAWN,         NULL}, // S_MERC_NULL
+	{S_MERC_SPAWN,             5,               S_MERC_WANDER,        M_SpawnThink}, // S_MERC_SPAWN
+	{S_MERC_WANDER,            ticrate_base,    S_MERC_IDLE,          M_WanderThink}, // S_MERC_WANDER
+	{S_MERC_IDLE,              ticrate_base,    S_MERC_WANDER,        M_IdleThink}, // S_MERC_IDLE
+	{S_MERC_CHASEPLAYR,        ticrate_base,    S_MERC_FIGHT,         M_ChasePlayr}, // S_MERC_CHASEPLAYR
+	{S_MERC_FIGHT,             ticrate_base,    S_MERC_WANDER,        M_FightThink}, // S_MERC_FIGHT
+	{S_MERC_FLEE,              ticrate_base,    S_MERC_WANDER,        M_FleeThink}, // S_MERC_FLEE
+	{S_MERC_DEAD,              ticrate_base,    S_MERC_NULL,          M_DeadThink}, // S_MERC_DEAD
+	{S_MERC_LEADER_NULL,       0,               S_MERC_LEADER_SPAWN,  NULL}, // S_MERC_LEADER_NULL
+	{S_MERC_LEADER_SPAWN,      5,               S_MERC_LEADER_WANDER, M_SpawnThink}, // S_MERC_LEADER_SPAWN
+	{S_MERC_LEADER_WANDER,     ticrate_base,    S_MERC_LEADER_IDLE,   M_WanderThink}, // S_MERC_LEADER_WANDER
+	{S_MERC_LEADER_IDLE,       ticrate_base,    S_MERC_LEADER_WANDER, M_IdleThink}, // S_MERC_LEADER_IDLE
+	{S_MERC_LEADER_CHASEPLAYR, ticrate_base,    S_MERC_LEADER_FIGHT,  M_ChasePlayr}, // S_MERC_LEADER_CHASEPLAYR
+	{S_MERC_LEADER_FIGHT,      ticrate_base,    S_MERC_LEADER_WANDER, M_FightThink}, // S_MERC_LEADER_FIGHT
+	{S_MERC_LEADER_FLEE,       ticrate_base,    S_MERC_LEADER_WANDER, M_FleeThink}, // S_MERC_LEADER_FLEE
+	{S_MERC_LEADER_DEAD,       ticrate_base,    S_MERC_LEADER_NULL,   M_DeadThink}, // S_MERC_LEADER_DEAD
+	{S_PISTOL_NULL,            0,               S_PISTOL_SPAWN,       NULL}, // S_PISTOL_NULL
+	{S_PISTOL_SPAWN,           5,               S_PISTOL_WANDER,      M_SpawnThink}, // S_PISTOL_SPAWN
+	{S_PISTOL_WANDER,          ticrate_base,    S_PISTOL_IDLE,        M_WanderThink}, // S_PISTOL_WANDER
+	{S_PISTOL_IDLE,            ticrate_base,    S_PISTOL_WANDER,      M_IdleThink}, // S_PISTOL_IDLE
+	{S_PISTOL_CHASEPLAYR,      ticrate_base,    S_PISTOL_FIGHT,       M_ChasePlayr}, // S_PISTOL_CHASEPLAYR
+	{S_PISTOL_FIGHT,           ticrate_base,    S_PISTOL_WANDER,      M_FightThink}, // S_PISTOL_FIGHT
+	{S_PISTOL_FLEE,            ticrate_base,    S_PISTOL_WANDER,      M_FleeThink}, // S_PISTOL_FLEE
+	{S_PISTOL_DEAD,            ticrate_base,    S_PISTOL_NULL,        M_DeadThink}, // S_PISTOL_DEAD
+	{S_SHOTTY_NULL,            0,               S_SHOTTY_SPAWN,       NULL}, // S_SHOTTY_NULL
+	{S_SHOTTY_SPAWN,           5,               S_SHOTTY_WANDER,      M_SpawnThink}, // S_SHOTTY_SPAWN
+	{S_SHOTTY_WANDER,          ticrate_base,    S_SHOTTY_IDLE,        M_WanderThink}, // S_SHOTTY_WANDER
+	{S_SHOTTY_IDLE,            ticrate_base,    S_SHOTTY_WANDER,      M_IdleThink}, // S_SHOTTY_IDLE
+	{S_SHOTTY_CHASEPLAYR,      ticrate_base,    S_SHOTTY_FIGHT,       M_ChasePlayr}, // S_SHOTTY_CHASEPLAYR
+	{S_SHOTTY_FIGHT,           ticrate_base,    S_SHOTTY_WANDER,      M_FightThink}, // S_SHOTTY_FIGHT
+	{S_SHOTTY_FLEE,            ticrate_base,    S_SHOTTY_WANDER,      M_FleeThink}, // S_SHOTTY_FLEE
+	{S_SHOTTY_DEAD,            ticrate_base,    S_SHOTTY_NULL,        M_DeadThink}, // S_SHOTTY_DEAD
+	{S_GUNNER_NULL,            0,               S_GUNNER_SPAWN,       NULL}, // S_GUNNER_NULL
+	{S_GUNNER_SPAWN,           5,               S_GUNNER_WANDER,      M_SpawnThink}, // S_GUNNER_SPAWN
+	{S_GUNNER_WANDER,          ticrate_base,    S_GUNNER_IDLE,        M_WanderThink}, // S_GUNNER_WANDER
+	{S_GUNNER_IDLE,            ticrate_base,    S_GUNNER_WANDER,      M_IdleThink}, // S_GUNNER_IDLE
+	{S_GUNNER_CHASEPLAYR,      ticrate_base,    S_GUNNER_FIGHT,       M_ChasePlayr}, // S_GUNNER_CHASEPLAYR
+	{S_GUNNER_FIGHT,           ticrate_base,    S_GUNNER_WANDER,      M_FightThink}, // S_GUNNER_FIGHT
+	{S_GUNNER_FLEE,            ticrate_base,    S_GUNNER_WANDER,      M_FleeThink}, // S_GUNNER_FLEE
+	{S_GUNNER_DEAD,            ticrate_base,    S_GUNNER_NULL,        M_DeadThink}, // S_GUNNER_DEAD
 };
 
 // name, sprite, health, armor, mtype, etype, rng, chance-to-spawn, sndtol, sndarea,
 // smelltol, smellarea, sight range, hasmelee, hashitscan. hasprojectile, melee dmg,
 // hitscan dmg, hitscan range, projectile dmg, projectile range, mlore, mdrops
 const mobj_t mobinfo[NUMMOBS] = {
-
-{"Hulk\0",           'h', 487, 56, MT_HULK,    ET_MOB, 150, 44, SND_VLOW, 15, SMELL_VLOW, 10, 40},
-{"Ravager\0",        'r', 353, 40, MT_RAVAGER, ET_MOB, 43,  18, SND_LOW,  7,  SMELL_LOW,  10, 40},
-{"Grunt\0",          'z', 21,  11, MT_GRUNT,   ET_MOB, 210, 89, SND_LOW,  10, SMELL_LOW,  10, 44},
-{"Druid\0",          'd', 135, 42, MT_DRUID,        ET_MOB,},
-{"Sand Wurm\0",      'W', 3153,35, MT_SANDWURM,     ET_MOB,},
-{"Street Thug\0",    't',},
-{"Beggar\0",         'b',},
-{"Mercenary\0",      'm', 233, 78, MT_MERC,         ET_MOB, 30,  41, SND_LOW,  10, SMELL_LOW,  10, 40},
-{"Pistol Guy\0",     'p', 49,  18, MT_PISTOL,       ET_MOB, 160, 95, SND_LOW,  10, SMELL_LOW,  10, 40},
-{"Shotgun Dude\0",   's', 111, 55, MT_SHOTTY,       ET_MOB, 57,  66, SND_LOW,  10, SMELL_LOW,  10, 40},
-{"City Guard\0",     'c',},
-{"Sharpshooter\0",   's',},
-{"Heavy Gunner\0",   'g', 245, 69, MT_GUNNER,       ET_MOB, 111, 59, SND_LOW,  10, SMELL_LOW,  10, 40},
-{"Nomadic Warrior\0",'n', 389, 79, MT_NOMAD_WARRIOR, ET_MOB, 17,  9,  SND_LOW,  10, SMELL_LOW,  10, 40},
-{"Nomadic Leader\0", 'l', 644, 96, MT_NOMAD_LEADER,  ET_MOB, 8}
-
+{
+	"Hulk\0",
+	'h',
+	487,
+	56,
+	MT_HULK,
+	S_HULK_NULL,
+	ET_MOB,
+	150,
+	44,
+	SND_VLOW,
+	15,
+	SMELL_VLOW,
+	10,
+	40
+},
+{
+	"Ravager\0",
+	'r',
+	353,
+	40,
+	MT_RAVAGER,
+	S_RAV_NULL,
+	ET_MOB,
+	43,
+	18,
+	SND_LOW,
+	7,
+	SMELL_LOW, 
+	10,
+	40
+},
+{
+	"Grunt\0",
+	'z',
+	21,
+	11,
+	MT_GRUNT,
+	S_GRUNT_NULL,
+	ET_MOB,
+	210,
+	89,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	44
+},
+{
+	"Druid\0",
+	'd',
+	135,
+	42,
+	MT_DRUID,
+	S_DRUID_NULL,
+	ET_MOB,
+},
+{
+	"Sand Wurm\0",
+	'W',
+	3153,
+	35,
+	MT_SANDWURM,
+	S_SANDWURM_NULL,
+	ET_MOB,
+},
+{
+	"Mercenary\0",
+	'm',
+	233,
+	78,
+	MT_MERC,
+	S_MERC_NULL,
+	ET_MOB,
+	30,
+	41,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	40
+},
+{
+	"Pistol Guy\0",
+	'p',
+	49,
+	18,
+	MT_PISTOL,
+	S_PISTOL_NULL,
+	ET_MOB,
+	160,
+	95,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	40
+},
+{
+	"Shotgun Dude\0",
+	's',
+	111,
+	55,
+	MT_SHOTTY,
+	S_SHOTTY_NULL,
+	ET_MOB,
+	57,
+	66,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	40
+},
+{
+	"Heavy Gunner\0",
+	'g',
+	245,
+	69,
+	MT_GUNNER,
+	S_GUNNER_NULL,
+	ET_MOB,
+	111,
+	59,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	40
+},
+{
+	"Nomadic Warrior\0",
+	'n',
+	389,
+	79,
+	MT_NOMAD_WARRIOR,
+	S_NOMAD_WARRIOR_NULL,
+	ET_MOB,
+	17,
+	9,
+	SND_LOW,
+	10,
+	SMELL_LOW,
+	10,
+	40
+},
+{
+	"Nomadic Leader\0",
+	'l',
+	644,
+	96,
+	MT_NOMAD_LEADER,
+	S_NOMAD_LEADER_NULL,
+	ET_MOB,
+	8
+}
 };
 
 /*
