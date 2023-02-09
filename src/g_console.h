c35ba7061f22c1c27e51adfc3c31d9d502d5b8ea
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2023-02-09 13:39:27 -0600

    Update and rename g_cvar.h to g_console.h

--- src/g_console.h
+++ src/g_console.h
@@ -1,12 +1,14 @@
-#ifndef _G_CVAR_
-#define _G_CVAR_
+#ifndef _G_CONSOLE_
+#define _G_CONSOLE_
 
 typedef struct _console_var
 {
 	const char* varstr;
 	std::string value;
+	void* varptr;
 } cvar_t;
 
+extern constexpr const char* commands[];
 extern cvar_t cvars[];
 
 #endif
