d7224b1125a2cbca191d6bb3cc45fa90e19334e9
SIGAAMDAD <86020846+SIGAAMDAD@users.noreply.github.com>
2023-02-15 09:10:47 -0600

    Create n_cheats.h

---
+++ src/n_cheats.h
@@ -0,0 +1,13 @@
+#ifndef _N_CHEATS_
+#define _N_CHEATS_
+
+#define MAGIC_XOR 300
+
+#define CHEAT_1 "JUEM" // fyia (f*** yeah! infinite ammo)
+#define CHEAT_2 "E[XNMK" // iwtbag (i want to be a god)
+#define CHEAT_3 "XKHBIO" // tgdnec (that god-damned never ending clip)
+#define CHEAT_4 "EMMKAJ" // iaagmf (i am a greedy m*****-f*****)
+
+nomadbool_t G_CheckCheat(const char* str);
+
+#endif
